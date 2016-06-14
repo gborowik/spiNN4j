04d7c546234632f4808c836e945961301bfca931
gborowik <gborowik@tele.pw.edu.pl>
2016-06-15 11:44:46 +1200

    file name changed

--- io/src/main/java/com/spiNN4j/io/parser/csvParser.java
+++ io/src/main/java/com/spiNN4j/io/parser/csvParser.java
@@ -4,5 +4,5 @@ package com.spiNN4j.io.parser;
  * Created by Grzegorz Borowik on 2016-06-13 4:30 PM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class csvParser {
+public class CSVParser {
 }
--- io/src/main/java/com/spiNN4j/io/writer/csvWriter.java
+++ io/src/main/java/com/spiNN4j/io/writer/csvWriter.java
@@ -4,5 +4,5 @@ package com.spiNN4j.io.writer;
  * Created by Grzegorz Borowik on 2016-06-13 4:31 PM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class csvWriter {
+public class CSVWriter {
 }
