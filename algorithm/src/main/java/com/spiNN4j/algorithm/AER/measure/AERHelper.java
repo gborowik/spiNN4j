e0902f3bee1622e9c39eb16a202fafdb33691515
gborowik <gborowik@tele.pw.edu.pl>
2016-06-18 00:18:05 +1200

    AERAlgorithm update

--- algorithm/src/main/java/com/spiNN4j/algorithm/AER/measure/AERHelper.java
+++ algorithm/src/main/java/com/spiNN4j/algorithm/AER/measure/AERHelper.java
@@ -4,5 +4,5 @@ package com.spiNN4j.algorithm.AER.measure;
  * Created by Grzegorz Borowik on 2016-06-17 2:54 PM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class AERMeasureHelper {
+public class AERHelper {
 }
