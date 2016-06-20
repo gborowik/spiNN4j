1839161c14f4ceb18a89605a7a355ac55add046a
gborowik <gborowik@tele.pw.edu.pl>
2016-06-20 17:38:57 +1200

    AER update

---
+++ algorithm/src/main/java/com/spiNN4j/algorithm/AER/algorithm/Helper.java
@@ -0,0 +1,41 @@
+package com.spiNN4j.algorithm.AER.algorithm;
+
+import com.codepoetics.protonpack.StreamUtils;
+import com.spiNN4j.model.algorithm.SpikeType;
+import com.spiNN4j.model.data.DataMatrix;
+import com.spiNN4j.model.data.DataVector;
+import one.util.streamex.StreamEx;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.spiNN4j.model.algorithm.SpikeType.generateSpike;
+
+/**
+ * Created by Grzegorz Borowik on 2016-06-20 4:08 PM.
+ * e-mail: borowik.grzegorz@gmail.com
+ */
+public class Helper {
+
+    public DataMatrix<SpikeType> generateSpikesForSuccessiveRowsOfSample(
+            DataMatrix<Double> data,
+            DataVector<Double> threshold
+    ) {
+        List<DataVector<SpikeType>> spikes = StreamEx.of(data.getData())
+                .pairMap((first, second) -> generateSpikesForTwoRowsOfSample(first, second, threshold))
+                .collect(Collectors.toList());
+
+        return new DataMatrix<>(spikes, spikes.get(0).size());
+    }
+
+    public DataVector<SpikeType> generateSpikesForTwoRowsOfSample(
+            DataVector<Double> first,
+            DataVector<Double> second,
+            DataVector<Double> threshold
+    ) {
+        return new DataVector<>(StreamUtils.zip(
+                first.getData().stream(), second.getData().stream(), threshold.getData().stream(),
+                (a, b, tr) -> generateSpike(a - b, tr))
+                .collect(Collectors.toList()));
+    }
+}
---
+++ model/src/main/java/com/spiNN4j/model/algorithm/SpikeType.java
@@ -0,0 +1,54 @@
+package com.spiNN4j.model.algorithm;
+
+/**
+ * Created by Grzegorz Borowik on 2016-06-20 4:11 PM.
+ * e-mail: borowik.grzegorz@gmail.com
+ */
+public enum SpikeType {
+    NEGATIVE_SPIKE {
+        @Override
+        public SpikeType getSpikeType() {
+            return SpikeType.NEGATIVE_SPIKE;
+        }
+
+        @Override
+        protected boolean isSpikeType(double signalDifference, double threshold) {
+            return signalDifference < -threshold;
+        }
+    },
+    POSITIVE_SPIKE {
+        @Override
+        public SpikeType getSpikeType() {
+            return SpikeType.POSITIVE_SPIKE;
+        }
+
+        @Override
+        protected boolean isSpikeType(double signalDifference, double threshold) {
+            return signalDifference > threshold;
+        }
+    },
+    NO_SPIKE {
+        @Override
+        public SpikeType getSpikeType() {
+            return SpikeType.NO_SPIKE;
+        }
+
+        @Override
+        protected boolean isSpikeType(double signalDifference, double threshold) {
+            return (-threshold <= signalDifference) && (signalDifference <= threshold);
+        }
+    };
+
+    public abstract SpikeType getSpikeType();
+
+    protected abstract boolean isSpikeType(double signalDifference, double threshold);
+
+    public static SpikeType generateSpike(Double signal, Double threshold) {
+        for (SpikeType spikeType : SpikeType.values()) {
+            if (spikeType.isSpikeType(signal, threshold)) {
+                return spikeType.getSpikeType();
+            }
+        }
+        throw new RuntimeException("Cannot generate spike");
+    }
+}
