aabab2e898e1c902f1e17cd8603ff18c32d2792c
gborowik <gborowik@tele.pw.edu.pl>
2016-06-23 21:44:20 +1200

    Neuron update

--- model/src/main/java/com/spiNN4j/model/data/neural/Neuron.java
+++ model/src/main/java/com/spiNN4j/model/data/neural/Neuron.java
@@ -6,20 +6,19 @@ import java.util.Set;
  * Created by Grzegorz Borowik on 2016-06-23 4:52 PM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class Neuron {
+public abstract class Neuron {
 
-    private Set<Synapse> preSynapses;
-    private Set<Synapse> postSynapses;
+    protected Set<Synapse> preSynapses;
+    protected Set<Synapse> postSynapses;
 
     public Neuron addPreSynapse(Synapse synapse) {
-        preSynapses.add(synapse);
+        this.preSynapses.add(synapse);
         return this;
     }
 
     public Neuron addPostSynapse(Synapse synapse) {
-        postSynapses.add(synapse);
+        this.postSynapses.add(synapse);
         return this;
     }
-
 
 }
