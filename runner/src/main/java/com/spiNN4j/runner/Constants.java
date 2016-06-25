bec7683eaa44a4634281fa4137b3155c74d15949
gborowik <gborowik@tele.pw.edu.pl>
2016-06-26 01:40:45 +1200

    Constants class created

---
+++ runner/src/main/java/com/spiNN4j/runner/Constants.java
@@ -0,0 +1,11 @@
+package com.spiNN4j.runner;
+
+/**
+ * Created by Grzegorz Borowik on 2016-06-26 12:56 AM.
+ * e-mail: borowik.grzegorz@gmail.com
+ */
+public class Constants {
+
+    public final static Double SPIKE = 1.0d;
+
+}
