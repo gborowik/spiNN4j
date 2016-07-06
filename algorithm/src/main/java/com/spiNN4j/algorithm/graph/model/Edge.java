54ce4506b1bdad2c429bdef0abcc825ad9f2781c
gborowik <gborowik@tele.pw.edu.pl>
2016-07-07 11:58:32 +1200

    Edge update

--- algorithm/src/main/java/com/spiNN4j/algorithm/graph/model/Edge.java
+++ algorithm/src/main/java/com/spiNN4j/algorithm/graph/model/Edge.java
@@ -1,13 +1,13 @@
 package com.spiNN4j.algorithm.graph.model;
 
-import com.spiNN4j.algorithm.graph.algorithm.AllPairs;
 import com.spiNN4j.algorithm.graph.algorithm.ConnectionGenerator.Factory;
+import com.spiNN4j.model.data.Pair;
 
 /**
  * Created by Grzegorz Borowik on 2016-07-04 3:15 PM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class Edge<T> extends AllPairs.Pair<T> {
+public class Edge<T> extends Pair<T> {
 
     public Edge(T first, T second) {
         super(first, second);
