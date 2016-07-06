5a96c0c89eeff48313213a0c5d5b40502fca3475
gborowik <gborowik@tele.pw.edu.pl>
2016-07-07 11:11:33 +1200

    test updated

---
+++ algorithm/src/test/java/com/spiNN4j/algorithm/graph/algorithm/AllPairsTest.groovy
@@ -0,0 +1,35 @@
+package com.spiNN4j.algorithm.graph.algorithm
+
+import spock.lang.Specification
+
+class AllPairsTest extends Specification {
+    def "AllPairs should return all pairs"() {
+        given:
+        List<String> allPairs = AllPairs.allPairs(list).collect(this.&concat)
+        expect:
+        allPairs.containsAll(expected)
+        where:
+        list            | expected
+        ['1', '2']      | ['1 2']
+        ['1']           | []
+        ['1', '2', '3'] | ['1 2', '1 3', '2 3']
+    }
+
+    def "AllPairs should have a proper number of elements"() {
+        given:
+        List<String> allPairs = AllPairs.allPairs(list).collect(this.&concat)
+        expect:
+        allPairs.size() == expected
+        where:
+        list       | expected
+        ['1'] * 1  | 0
+        ['1'] * 2  | 2 * 1 / 2
+        ['1'] * 3  | 3 * 2 / 2
+        ['1'] * 4  | 4 * 3 / 2
+        ['1'] * 20 | 20 * 19 / 2
+    }
+
+    private String concat(AllPairs.Pair<String> pair) {
+        return "${pair.first} ${pair.second}".toString()
+    }
+}
