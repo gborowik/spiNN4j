0fc26469ea829535fe47554cacaee3e3bd6bd6c0
gborowik <gborowik@tele.pw.edu.pl>
2016-07-07 21:21:59 +1200

    StreamOfSamplesAsDouble update

--- io/src/main/java/com/spiNN4j/io/model/StreamOfSamplesAsDouble.java
+++ io/src/main/java/com/spiNN4j/io/model/StreamOfSamplesAsDouble.java
@@ -8,11 +8,11 @@ import java.util.stream.Stream;
  * Created by Grzegorz Borowik on 2016-06-19 2:10 AM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class StreamOfSamples {
+public class StreamOfSamplesAsDouble {
 
     private Stream<DataMatrix<Double>> samples;
 
-    public StreamOfSamples(Stream<DataMatrix<Double>> samples) {
+    public StreamOfSamplesAsDouble(Stream<DataMatrix<Double>> samples) {
         this.samples = samples;
     }
 
