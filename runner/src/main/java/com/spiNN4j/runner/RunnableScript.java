04b53e9821de5795c578c52f2d305a5f77959060
gborowik <gborowik@tele.pw.edu.pl>
2016-07-07 23:49:26 +1200

    RunnableScript update

--- runner/src/main/java/com/spiNN4j/runner/RunnableScript.java
+++ runner/src/main/java/com/spiNN4j/runner/RunnableScript.java
@@ -1,5 +1,10 @@
 package com.spiNN4j.runner;
 
+import com.spiNN4j.io.ioManager.FolderManager;
+import com.spiNN4j.io.model.StreamOfSamplesAsDouble;
+import com.spiNN4j.io.parser.CSVParserDouble;
+import groovy.lang.Script;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,8 +14,17 @@ import java.net.URL;
  * Created by Grzegorz Borowik on 2016-06-14 3:07 PM.
  * e-mail: borowik.grzegorz@gmail.com
  */
-public class RunnableScript {
+public abstract class RunnableScript extends Script {
 
+    public static StreamOfSamplesAsDouble getData(String folder, String mask) {
+
+        return new StreamOfSamplesAsDouble(
+                new FolderManager(folder)
+                        .inputStreamsFilesMatch(mask)
+                        .map(file -> new CSVParserDouble(file).parse())
+        );
+
+    }
 
 
     public static FileInputStream inputStreamFrom(String name) {
